67e49402227eb74dcd4b6e4a59f99a11a8b1ccf3
Ned Wolpert <ned.wolpert@gmail.com>
2022-05-30 09:41:47 -0700

    Added the ability to include additional dependencies for the resolver.
    This is needed in cases you want to resolve with utilities that need configured classes.
    Example: DynamoDB needs to be injected really. You could do weird things with the
    credentials but that's not the best way through this. You would not need this for
    JDBC drivers as that can be done completely via string variables. But there are so
    many ways to configure DDB that you may as well just get the real thing. Either way,
    it adds to the capability.

--- oop-mock-common/src/main/java/com/codeheadsystems/oop/mock/dagger/ResolverModule.java
+++ oop-mock-common/src/main/java/com/codeheadsystems/oop/mock/dagger/ResolverModule.java
@@ -21,16 +21,15 @@ import com.codeheadsystems.oop.ResolverConfiguration;
 import com.codeheadsystems.oop.mock.Hasher;
 import com.codeheadsystems.oop.mock.converter.JsonConverter;
 import com.codeheadsystems.oop.mock.manager.ResourceLookupManager;
-import com.codeheadsystems.oop.mock.resolver.MockDataResolver;
-import com.codeheadsystems.oop.mock.resolver.ResolverFactory;
 import com.codeheadsystems.oop.mock.translator.Translator;
+import com.google.common.collect.ImmutableMap;
 import dagger.Binds;
 import dagger.BindsOptionalOf;
 import dagger.Module;
 import dagger.Provides;
 import dagger.multibindings.ClassKey;
 import dagger.multibindings.IntoMap;
-import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 import java.util.Optional;
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -40,38 +39,40 @@ import javax.inject.Singleton;
  *
  * @see com.codeheadsystems.oop.mock.resolver.ResolverFactory
  */
-@Module(includes = ResolverModule.ResolverNameModule.class)
+@Module(includes = {ResolverModule.ResolverConfigModule.class})
 public interface ResolverModule {
 
     String RESOLVER_MAP = "resolver_map";
+    String RESOLVER_INTERNAL_MAP = "resolver internal map";
     String DEFAULT_RESOLVER = "DEFAULT RESOLVER";
     String RESOLVER_CLASSNAME = "RESOLVER CLASSNAME";
+    String RESOLVER_ADDITIONAL_DEPS = "Additional_Dependencies";
 
-    @Named(RESOLVER_MAP)
+    @Named(RESOLVER_INTERNAL_MAP)
     @Binds
     @IntoMap
     @ClassKey(OopMockConfiguration.class)
     Object bindsOopMockConfiguration(OopMockConfiguration configuration);
 
-    @Named(RESOLVER_MAP)
+    @Named(RESOLVER_INTERNAL_MAP)
     @Binds
     @IntoMap
     @ClassKey(JsonConverter.class)
     Object bindsJsonConverter(JsonConverter converter);
 
-    @Named(RESOLVER_MAP)
+    @Named(RESOLVER_INTERNAL_MAP)
     @Binds
     @IntoMap
     @ClassKey(ResourceLookupManager.class)
     Object bindsResourceLookupManager(ResourceLookupManager manager);
 
-    @Named(RESOLVER_MAP)
+    @Named(RESOLVER_INTERNAL_MAP)
     @Binds
     @IntoMap
     @ClassKey(Translator.class)
     Object bindsTranslator(Translator translator);
 
-    @Named(RESOLVER_MAP)
+    @Named(RESOLVER_INTERNAL_MAP)
     @Binds
     @IntoMap
     @ClassKey(Hasher.class)
@@ -82,7 +83,37 @@ public interface ResolverModule {
     String defaultResolver();
 
     @Module
-    class ResolverNameModule {
+    class ResolverConfigModule {
+
+        private final Map<Class<?>, Object> additionalDependencies;
+
+        public ResolverConfigModule() {
+            this(ImmutableMap.of());
+        }
+
+        public ResolverConfigModule(final Map<Class<?>, Object> additionalDependencies) {
+            this.additionalDependencies = additionalDependencies;
+        }
+
+        @Provides
+        @Singleton
+        @Named(RESOLVER_MAP)
+        public Map<Class<?>, Object> fullDependencies(@Named(RESOLVER_ADDITIONAL_DEPS) final Map<Class<?>, Object> addition,
+                                                      @Named(RESOLVER_INTERNAL_MAP) final Map<Class<?>, Object> internal,
+                                                      final OopMockConfiguration configuration) {
+            final ImmutableMap.Builder<Class<?>, Object> builder = ImmutableMap.builder();
+            builder.putAll(internal);
+            builder.putAll(addition);
+            configuration.resolverConfiguration().ifPresent(rc -> builder.put(ResolverConfiguration.class, rc));
+            return builder.build();
+        }
+
+        @Provides
+        @Singleton
+        @Named(RESOLVER_ADDITIONAL_DEPS)
+        public Map<Class<?>, Object> additionalDependencies() {
+            return additionalDependencies;
+        }
 
         @Provides
         @Singleton
