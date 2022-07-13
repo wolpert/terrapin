8c3f744b4af071d376ea301dcbe1f912eb82fe56
Ned Wolpert <ned.wolpert@gmail.com>
2022-07-13 07:52:04 -0700

    Added skeleton metrics into dao

--- keyservice-dao-ddb/src/main/java/com/codeheadsystems/terrapin/server/dao/KeyDAODynamoDB.java
+++ keyservice-dao-ddb/src/main/java/com/codeheadsystems/terrapin/server/dao/KeyDAODynamoDB.java
@@ -16,6 +16,7 @@
 
 package com.codeheadsystems.terrapin.server.dao;
 
+import com.codeheadsystems.metrics.Metrics;
 import com.codeheadsystems.terrapin.server.dao.accessor.DynamoDbClientAccessor;
 import com.codeheadsystems.terrapin.server.dao.converter.KeyConverter;
 import com.codeheadsystems.terrapin.server.dao.model.Batch;
@@ -23,7 +24,9 @@ import com.codeheadsystems.terrapin.server.dao.model.Key;
 import com.codeheadsystems.terrapin.server.dao.model.KeyIdentifier;
 import com.codeheadsystems.terrapin.server.dao.model.KeyVersionIdentifier;
 import com.codeheadsystems.terrapin.server.dao.model.OwnerIdentifier;
+import io.micrometer.core.instrument.Timer;
 import java.util.Optional;
+import java.util.function.Supplier;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.slf4j.Logger;
@@ -36,89 +39,126 @@ import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
 
 @Singleton
 public class KeyDAODynamoDB implements KeyDAO {
+    public static final String OWNER = "owner";
     private static final Logger LOGGER = LoggerFactory.getLogger(KeyDAODynamoDB.class);
-
     private final TableConfiguration tableConfiguration;
     private final DynamoDbClientAccessor dynamoDbClientAccessor;
     private final KeyConverter keyConverter;
+    private final Metrics metrics;
 
     @Inject
     public KeyDAODynamoDB(final DynamoDbClientAccessor dynamoDbClientAccessor,
                           final TableConfiguration tableConfiguration,
-                          final KeyConverter keyConverter) {
+                          final KeyConverter keyConverter,
+                          final Metrics metrics) {
         LOGGER.info("KeyDAODynamoDB({},{},{})", dynamoDbClientAccessor, tableConfiguration, keyConverter);
         this.tableConfiguration = tableConfiguration;
         this.dynamoDbClientAccessor = dynamoDbClientAccessor;
         this.keyConverter = keyConverter;
+        this.metrics = metrics;
+    }
+
+    private <T> T time(final String methodName,
+                       final String owner,
+                       final Supplier<T> supplier) {
+        final String name = "ddbdao." + methodName;
+        final Timer timer;
+        if (owner != null) {
+            timer = metrics.registry().timer(name, OWNER, owner); // TODO: Vet cardinality. Set by configuration?
+        } else {
+            timer = metrics.registry().timer(name);
+        }
+        return metrics.time(name, timer, supplier);
     }
 
     @Override
     public void store(final Key key) {
         LOGGER.debug("store({})", key.keyIdentifier());
-        final PutItemRequest request = keyConverter.toPutItemRequest(key);
-        final PutItemResponse response = dynamoDbClientAccessor.putItem(request);
-        final ConsumedCapacity consumedCapacity = response.consumedCapacity();
-        LOGGER.debug("store:{}", consumedCapacity);
+        time("store", key.keyIdentifier().owner(), () -> {
+            final PutItemRequest request = keyConverter.toPutItemRequest(key);
+            final PutItemResponse response = dynamoDbClientAccessor.putItem(request);
+            final ConsumedCapacity consumedCapacity = response.consumedCapacity();
+            LOGGER.debug("store:{}", consumedCapacity);
+            return null;
+        });
     }
 
     @Override
     public Optional<Key> load(final KeyVersionIdentifier identifier) {
         LOGGER.debug("load({})", identifier);
-        final GetItemRequest request = keyConverter.toGetItemRequest(identifier);
-        final GetItemResponse response = dynamoDbClientAccessor.getItem(request);
-        if (response.hasItem()) {
-            return Optional.of(keyConverter.from(response));
-        } else {
-            return Optional.empty();
-        }
+        return time("loadversion", identifier.owner(), () -> {
+            final GetItemRequest request = keyConverter.toGetItemRequest(identifier);
+            final GetItemResponse response = dynamoDbClientAccessor.getItem(request);
+            if (response.hasItem()) {
+                return Optional.of(keyConverter.from(response));
+            } else {
+                return Optional.empty();
+            }
+        });
     }
 
     @Override
     public Optional<Key> load(final KeyIdentifier identifier) {
         LOGGER.debug("load({})", identifier);
-        return Optional.empty();
+        return time("loadkey", identifier.owner(), () -> {
+            return Optional.empty();
+        });
     }
 
     @Override
     public Optional<OwnerIdentifier> loadOwner(final String ownerName) {
         LOGGER.debug("loadOwner({})", ownerName);
-        return Optional.empty();
+        return time("loadowner", ownerName, () -> {
+            return Optional.empty();
+        });
     }
 
     @Override
     public Batch<OwnerIdentifier> listOwners() {
         LOGGER.debug("listOwners()");
-        return null;
+        return time("listowners", null, () -> {
+            return null;
+        });
     }
 
 
     @Override
     public Batch<KeyIdentifier> listKeys(final OwnerIdentifier identifier) {
         LOGGER.debug("listKeys({})", identifier);
-        return null;
+        return time("listkeys", identifier.owner(), () -> {
+            return null;
+        });
     }
 
     @Override
     public Batch<KeyVersionIdentifier> listVersions(final KeyIdentifier identifier) {
         LOGGER.debug("listVersions({})", identifier);
-        return null;
+        return time("listversions", identifier.owner(), () -> {
+            return null;
+        });
     }
 
     @Override
     public boolean delete(final KeyVersionIdentifier identifier) {
         LOGGER.debug("delete({})", identifier);
-        return false;
+        return time("deleteversions", identifier.owner(), () -> {
+            return false;
+        });
     }
 
     @Override
     public boolean delete(final KeyIdentifier identifier) {
         LOGGER.debug("delete({})", identifier);
-        return false;
+        return time("deletekey", identifier.owner(), () -> {
+            return false;
+        });
     }
 
     @Override
     public boolean delete(final OwnerIdentifier identifier) {
         LOGGER.debug("delete({})", identifier);
-        return false;
+        return time("deleteowner", identifier.owner(), () -> {
+            return false;
+        });
     }
 }
